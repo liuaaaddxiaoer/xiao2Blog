467ce27666f4bcc05bab9124a438f400aa41cbc2
liuxiaoer@offcn.com <liuxiaoer@offcn.com>
2019-11-24 15:52:41 +0800

    gzip

--- client/blog/config/index.js
+++ client/blog/config/index.js
@@ -32,7 +32,7 @@ module.exports = {
     // https://vue-loader.vuejs.org/en/options.html#cachebusting
     cacheBusting: true,
 
-    cssSourceMap: false
+    cssSourceMap: true
   },
 
   build: {
@@ -56,7 +56,7 @@ module.exports = {
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
     // npm install --save-dev compression-webpack-plugin
-    productionGzip: false,
+    productionGzip: true,
     productionGzipExtensions: ['js', 'css'],
 
     // Run the build command with an extra argument to
