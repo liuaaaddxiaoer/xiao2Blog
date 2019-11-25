12f98b687596482e297f4f5e6d0e2a95b9ce2a8c
liuxiaoer@offcn.com <liuxiaoer@offcn.com>
2019-11-25 16:23:09 +0800

    url 匹配bug

--- server/blog/urls.py
+++ server/blog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, re_path
 from blog.views import CreateArticle, ArticleList, CategoryList
 
-urlpatterns = {
+urlpatterns = [
     path('createArticle/l', CreateArticle.as_view()),  # 创建/更新文章
     re_path('articleList/?', ArticleList.as_view()),  # 获取文章列表
     re_path('categoryList/?', CategoryList.as_view()),  # 获取文章列表
-}
+]
