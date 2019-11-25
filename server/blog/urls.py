d15618a51659efd8ab587426d9847413a50b7799
liuxiaoer@offcn.com <liuxiaoer@offcn.com>
2019-11-25 15:50:20 +0800

    增加前缀

--- server/blog/urls.py
+++ server/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path
 from blog.views import CreateArticle, ArticleList, CategoryList
 
 urlpatterns = {
-    path('createArticle/', CreateArticle.as_view()),  # 创建/更新文章
+    path('createArticle/l', CreateArticle.as_view()),  # 创建/更新文章
     re_path('articleList/?', ArticleList.as_view()),  # 获取文章列表
     re_path('categoryList/?', CategoryList.as_view()),  # 获取文章列表
 }
--- server/server/urls.py
+++ server/server/urls.py
@@ -19,6 +19,6 @@ from blog import urls
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(urls))
+    path('api2/admin/', admin.site.urls),
+    path('api2/api/', include(urls))
 ]
