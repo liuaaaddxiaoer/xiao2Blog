02323a0e03ca4583a8f4b607f19edef6dafd1de6
liuxiaoer@offcn.com <liuxiaoer@offcn.com>
2019-11-26 14:13:51 +0800

    vue 接口axios封装

--- server/server/settings.py
+++ server/server/settings.py
@@ -55,12 +55,12 @@ MIDDLEWARE = [
 
 
 CORS_ORIGIN_ALLOW_ALL = True #接收所有跨域来源
-CORS_ORIGIN_WHITELIST = (
-    'google.com',
-    'hostname.example.com',
-    'localhost:8080',
-    '127.0.0.1:9000'
-)
+# CORS_ORIGIN_WHITELIST = (
+#     'google.com',
+#     'hostname.example.com',
+#     'localhost:8080',
+#     '127.0.0.1:9000'
+# )
 
 
 ROOT_URLCONF = 'server.urls'
