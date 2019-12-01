2891cc51aa51d51cd15470319599e2e09f3a7be8
liuxiaoer@offcn.com <liuxiaoer@offcn.com>
2019-12-01 14:24:55 +0800

    环境判断3

--- client/blog/src/http/url.js
+++ client/blog/src/http/url.js
@@ -1,9 +1,7 @@
-import {NODE_ENV} from '../../config/dev.env.js'
-
-let isDev = NODE_ENV.indexOf(process.env.NODE_ENV)
 
 
-export const BASE_URL = isDev ? 'http://127.0.0.1:8000/api2/' : 'https://www.liuxiaoer.club/api2/'
+
+export const BASE_URL = process.env.NODE_ENV != 'production' ? 'http://127.0.0.1:8000/api2/' : 'https://www.liuxiaoer.club/api2/'
 
 export const ARTICLE_LIST = 'api/articleList'
 
