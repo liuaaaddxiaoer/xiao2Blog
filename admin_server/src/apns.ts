a32a43d83a78a844935ac5ecc0e1bf7dac1e2db0
liuxiaoer@offcn.com <oneenddi@163.com>
2020-01-11 12:18:31 +0800

    apns开发

--- admin_server/src/apns.ts
+++ admin_server/src/apns.ts
@@ -1,1 +1,22 @@
-console.log('apns')
+import Apn from 'apn'
+import path from 'path'
+
+
+// p8鉴权
+let token = {
+  key: path.resolve(__dirname, '../static', 'AuthKey_3BP626372L.p8'),
+  keyId: '3BP626372L',
+  teamId: '3BP626372L'
+}
+
+
+// 设置privider
+const privider = new Apn.Provider({
+  token: token
+})
+
+// 设置推送内容
+const notifation = new Apn.Notification()
+notifation.topic = 'com.xiaoer.001'
+
+privider.send(notifation, '1')
--- client/blog/tool/Pages/Apns.vue
+++ client/blog/tool/Pages/Apns.vue
@@ -1,15 +1,161 @@
 <template>
-  <div class="apns_container">我是iOS推送</div>
+  <div class="apns_section_container">
+    <h3>iOS推送 支持最新P8，支持P12</h3>
+
+    <!-- 鉴权 -->
+    <div class="token">
+      <div class="token_header">
+        <span>鉴权方式:</span>
+        <el-radio v-model="tokenType" label="1" border>Token Authentication (p8)</el-radio>
+        <i class="el-icon-question" />
+        <el-radio v-model="tokenType" label="2" border>APNs证书（P12）</el-radio>
+        <i class="el-icon-question" />
+      </div>
+    </div>
+    <!-- p8证书 -->
+    <transition name="fade">
+      <div class="p8_container" v-show="tokenType == 1">
+        <div class="item_container">
+          <span>Bundle ID</span>
+          <i class="el-icon-question"></i>
+          <el-input type="text" />
+        </div>
+
+        <div class="item_container">
+          <span>Key ID:</span>
+          <i class="el-icon-question" />
+          <el-input type="text" />
+        </div>
+
+        <div class="item_container">
+          <span>Team ID:</span>
+          <i class="el-icon-question" />
+          <el-input type="text" />
+        </div>
+
+        <div class="item_container">
+          <span>Auth key:</span>
+          <i class="el-icon-question" />
+          <!-- 上传组件 -->
+          <el-upload class="upload-demo" drag action="https://jsonpla">
+            <i class="el-icon-upload"></i>
+            <div class="el-upload__text">
+              将文件拖到此处，或
+              <em>点击上传</em>
+            </div>
+            <div class="el-upload__tip" slot="tip">只能上传.p8文件</div>
+          </el-upload>
+          <el-button type="primary" class="submit" round disabled>确认添加</el-button>
+        </div>
+      </div>
+    </transition>
+
+    <!-- apns证书 -->
+    <transition name="fade">
+      <div class="apns_container" v-show="tokenType == 2">
+        <div class="mode">
+          <el-radio v-model="mode" label="1" border>开发证书</el-radio>
+          <el-radio v-model="mode" label="2" border>生产证书</el-radio>
+        </div>
+
+        <div class="item_container">
+          <span>Bundle ID</span>
+          <i class="el-icon-question"></i>
+          <el-input type="text" />
+        </div>
+
+        <div class="item_container">
+          <span>证书密码</span>
+          <i class="el-icon-question"></i>
+          <el-input type="text" />
+        </div>
+        <div class="item_container">
+          <span>Apns证书:</span>
+          <i class="el-icon-question" />
+          <!-- 上传组件 -->
+          <el-upload class="upload-demo" drag action="https://jsonpla">
+            <i class="el-icon-upload"></i>
+            <div class="el-upload__text">
+              将文件拖到此处，或
+              <em>点击上传</em>
+            </div>
+            <div class="el-upload__tip" slot="tip">只能上传.p12文件</div>
+          </el-upload>
+          <el-button type="primary" class="submit" round disabled>确认添加</el-button>
+        </div>
+      </div>
+    </transition>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Apns",
   data() {
-    return {};
+    return {
+      tokenType: "1", // 鉴权方式 1是tokenP8 2 是apns
+      mode: "1" // apns 1是dev 2是dis
+    };
   }
 };
 </script>
 
 <style lang="less" scoped>
+.apns_section_container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 100px;
+  padding-top: 50px;
+  h3 {
+    margin-bottom: 50px;
+  }
+}
+.token {
+  display: flex;
+  flex-direction: column;
+  .token_header {
+    display: flex;
+    align-items: center;
+  }
+}
+
+.el-icon-question {
+  font-size: 20px;
+  margin-left: -15px;
+  margin-right: 15px;
+  &:hover {
+    cursor: pointer;
+    color: #409eff;
+  }
+}
+
+.p8_container,
+.apns_container {
+  display: flex;
+  flex-direction: column;
+  margin-top: 40px;
+  border-radius: 4px;
+  padding: 30px;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+  .item_container {
+    padding: 20px 0;
+    span {
+      padding-right: 20px;
+    }
+    .submit {
+      margin-top: 20px;
+      width: 100%;
+    }
+  }
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
 </style>
